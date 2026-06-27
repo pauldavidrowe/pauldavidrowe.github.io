9eb35f264e941437244695cb51fe464914c9cdd1
Paul Rowe <pauldavidrowe@gmail.com>
2026-06-26 22:05:58 -0400

    update icon

--- spellingbee/sw.js
+++ spellingbee/sw.js
@@ -3,7 +3,7 @@
 // Caches app shell for offline use.
 // ============================================================
 
-const CACHE_NAME = "spellingbee-v2.5";
+const CACHE_NAME = "spellingbee-v2.6";
 const SHELL = [
   "./index.html",
   "./app.js",
